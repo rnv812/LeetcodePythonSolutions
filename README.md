d9537798b6ba01fc70409ea357b119656e5d8045
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-06-18 14:20:32 +0300

    Add "Palindrome Number" solution

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Solutions to leetcode problems in python
 | # | Title | Solution | Difficulty |
 |:-:| :---- | :------: | :--------: |
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [🔗](./solutions/easy/0001_two_sum.py) | 🟢 |
+| 9 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/) | [🔗](./solutions/easy/0009_palindrome_number.py) | 🟢 |
---
+++ solutions/easy/0009_palindrome_number.py
@@ -0,0 +1,47 @@
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+# Example 1:
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
+# Example 2:
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+# Example 3:
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+# Constraints:
+# -231 <= x <= 231 - 1
+
+# Follow up: Could you solve it without converting the integer to a string?
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        elif x < 10:
+            return True
+
+        digits = []
+        remains = x
+
+        while remains:
+            digits.append(remains % 10)
+            remains //= 10
+
+        left = 0
+        right = len(digits) - 1
+
+        while left < right:
+            if digits[left] != digits[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
